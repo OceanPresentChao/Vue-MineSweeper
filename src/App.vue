c4005be4260804e1bab8f6745aef5f71b71fd42b
OceanPresent <oceanpresent@163.com>
2022-08-01 21:36:08 +0800

    :construction: chore: update pnpm

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125">
     <div>
       <HelloWorld msg="You did it!" />
+      Hello
     </div>
   </main>
 </template>
